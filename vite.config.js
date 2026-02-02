caca5fc01d7b48bef39bdb21022cfd9a1b8eba62
mimsalman <2003mimsalman@gmail.com>
2026-02-02 16:13:27 +0530

    Update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    cacheDir: process.env.VITE_CACHE_DIR || 'node_modules/.vite',
+
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
